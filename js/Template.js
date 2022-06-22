4bea946c320022f3c99f287f8d5c4ec7d1812e45
cool9203 <octer18@gmail.com>
2022-06-22 12:38:34 +0800

    edit: add example for namespace code style

--- js/Template.js
+++ js/Template.js
@@ -4,6 +4,34 @@
  * @params {type} var2
  * @returns type
  */ 
-function funcrion_name(var1, ...){
+function funcrion_name(var1, ...vars){
 
+}
+
+
+namespace = {
+    /** 
+     * description
+     * @params {type} para1 description constructor paramater
+     * @params {type} para2 description constructor paramater
+     * @returns type
+     */ 
+    class_name : 
+    class class_name{
+        constructor(para1, ...paras){
+
+        }
+    }, 
+
+
+    /** 
+     * description
+     * @params {type} var1
+     * @params {type} var2
+     * @returns type
+     */ 
+    function_name : 
+    function function_name(var1, ...vars){
+
+    }
 }
