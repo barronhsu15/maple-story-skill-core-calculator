7899a76303f68cec4012da93b7a5ab22b83cfae5
cool9203 <octer18@gmail.com>
2022-06-22 12:39:41 +0800

    temp: 2022/06/22 12:41

--- js/ImageBase.js
+++ js/ImageBase.js
@@ -8,21 +8,46 @@ function convert_image_data_to_opencv_mat(canvas_id){
 
 
 /** 
+ * example for convert Image to cv.Mat
+ * @returns cv.Mat
+ */ 
+function get_image_topk_point(src, k, compare=(a, b) => a > b){
+    let topk = new util.insert_sort(k);
+
+    for (let row = 0; row < src.rows; row++){
+        for (let col = 0; row < src.cols; col++){
+            let value = src.row(row).col(col).data32F[0];
+
+            //init point and insert point to topk
+            let point = new util.point();
+            point.x = col;
+            point.y = row;
+            point.value = value;
+            topk.insert(point);
+        }
+    }
+}
+
+
+/** 
  * calc two images similarity.
  * @params {cv.Mat} src
  * @params {cv.Mat} templ
- * @returns double or [cv.point, double]
+ * @params {bool} image_index
+ * @params {bool} get_dst
+ * @returns cv.Mat or double or [cv.point, double]
  */ 
-function image_similarity(src, templ, image_index=false){
+function image_similarity(src, templ, image_index=false, k=1, get_dst=false){
     let dst = new cv.Mat();
-    let mask = new cv.Mat();
+    cv.matchTemplate(src, templ, dst, cv.TM_CCOEFF_NORMED);
 
-    cv.matchTemplate(src, templ, dst, cv.TM_CCOEFF_NORMED, mask);
+    if (get_dst == true){
+        return dst;
+    }
 
-    let result = cv.minMaxLoc(dst, mask);
+    let result = cv.minMaxLoc(dst);
     
     dst.delete();
-    mask.delete();
 
     if (image_index == undefined){
         return result.maxVal;
--- js/util.js
+++ js/util.js
@@ -1,9 +1,93 @@
-/** 
- * description
- * @params {array} array
- * @params {any type} target
- * @returns int  if target in array, will return idnex, else return -1
- */ 
-function search_array(array, target){
-    return array.findIndex((e) => e == target);
+var util = {
+    /** 
+     * 2 dim point and 1 value struct.
+     * @params {int/double} x
+     * @params {int/double} y
+     * @params {any type} value
+     */ 
+    point : 
+    class point{
+        constructor(x, y, value){
+            this.x = x;
+            this.y = y;
+            this.value = value;
+        }
+    },
+
+
+    /** 
+     * limit k object with insert sort.
+     * @params {int} k
+     * @params {function} compare
+     * @params {function} equal
+     */ 
+    insert_sort : 
+    class insert_sort{
+        constructor(k, compare=(a, b) => a > b, equal=(a, b) => a == b){
+            this.array = new Array();
+            this.k = k;
+            this.compare = compare;
+            this.equal = equal;
+        }
+
+        insert(obj){
+            let inserted = false;
+            for (let i = 0; i < this.array.length; i++){
+                if (this.compare(obj, this.array[i])){
+                    this.array.splice(i, 0, obj);
+                    inserted = true;
+                    break;
+                }
+            }
+
+            if (this.array.length > this.k){
+                this.array.pop();
+            }
+
+            if (!inserted && this.array.length < this.k){
+                this.array.push(obj);
+            }
+        }
+
+        get(index){
+            if (index < 0 || index == undefined){
+                return this.array;
+            }
+            return this.array[index];
+        }
+
+        find(obj){
+            for (let i = 0; i < this.array.length; i++){
+                if (this.equal(obj, this.array[i])){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        indexof(obj){
+            for (let i = 0; i < this.array.length; i++){
+                if (this.equal(obj, this.array[i])){
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        length(){
+            return this.array.length;
+        }
+    },
+
+
+    /** 
+     * description
+     * @params {array} array
+     * @params {any type} target
+     * @returns int  if target in array, will return idnex, else return -1
+     */ 
+    search_array : 
+    function search_array(array, target){
+        return array.findIndex((e) => e == target);
+    }
 }
