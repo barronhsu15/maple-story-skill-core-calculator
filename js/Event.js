52e9c5c8b13feabbdd82701610934976c3628fbe
Barron Hsu <barron.hsu.15@gmail.com>
2022-08-06 19:12:01 +0800

    Add event handler Event.js

---
+++ js/Event.js
@@ -0,0 +1,171 @@
+var _formJob = document.getElementById("form-job");
+var _formCoreskills = document.getElementById('form-coreskills');
+var _formSkillsContainer = document.getElementById("form-skills-container");
+var _formOwnedCoresContainer = document.getElementById("form-owned-cores-container");
+
+/**
+ * Set form-job options
+ */
+function setFormJob() {
+    for (let i in Jobs) {
+        let option = document.createElement("option");
+        option.setAttribute("value", i);
+        option.textContent = Jobs[i].name;
+        _formJob.append(option);
+    }
+}
+
+/**
+ * Set form-coreskills options
+ * 
+ * @param {string} job
+ */
+function setFormCoreskills(job) {
+    let quantity = Math.floor(Jobs[job].skills.length / Core.skillsInCore);
+
+    for (let i = 0; i < quantity; i++) {
+        let option = document.createElement("option");
+        option.setAttribute("value", i);
+        option.textContent = Core.perfect[i];
+        _formCoreskills.append(option);
+    }
+}
+
+/**
+ * Set form-skills-container checkbox
+ * 
+ * @param {string} job
+ */
+function setFormSkillsContainer(job) {
+    let div = document.createElement("div");
+    div.setAttribute("class", "sub-form-box");
+
+    for (let i = 0; i < Jobs[job].skills.length; i++) {
+        let span = document.createElement("span");
+        let checkbox = document.createElement("input");
+        let label = document.createElement("label");
+        let img = document.createElement("img");
+
+        checkbox.setAttribute("id", "form-skill-" + i.toString());
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.setAttribute("value", i);
+        checkbox.dataset.name = Jobs[job].skills[i];
+        label.setAttribute("for", "form-skill-" + i.toString());
+        label.textContent = Jobs[job].skills[i];
+        img.setAttribute("src", "images/jobs/" + job + "/skills/" + Jobs[job].skills[i] + ".png");  // Can be changed to config file and printf e.g. images/jobs/%s/skills/%s.png
+
+        span.append(checkbox);
+        span.append(label);
+        label.append(img);
+        div.append(span);
+
+        checkbox.addEventListener("change", function() {
+            console.log(this.checked);
+            skillsCheckboxController(job, this.dataset.name, this.checked);
+        });
+    }
+
+    _formSkillsContainer.append(div);
+}
+
+/**
+ * Set form-owned-cores-container checkbox
+ * 
+ * @param {string} job
+ * @param {string} skill
+ */
+function setFormOwnedCoresContainer(job, skill) {
+    console.log(job);
+    console.log(skill);
+    let div = document.createElement("div");
+    let label = document.createElement("label");
+    let details = document.createElement("details");
+    let summary = document.createElement("summary");
+    let hr = document.createElement("hr");
+
+    label.textContent = skill;
+    summary.append(label);
+    details.append(summary);
+    details.append(hr);
+    div.setAttribute("id", "form-owned-core-" + skill);
+    div.setAttribute("class", "sub-form-box");
+
+    for (let i = 0; i < Jobs[job].coreskills[skill].length; i++) {
+        let span = document.createElement("span");
+        let checkbox = document.createElement("input");
+        let label = document.createElement("label");
+        let img = document.createElement("img");
+
+        checkbox.setAttribute("id", "form-owned-core-" + skill + "-" + i.toString());
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.setAttribute("value", i);
+        label.setAttribute("for", "form-owned-core-" + skill + "-" + i.toString());
+        label.textContent = Jobs[job].coreskills[skill][i];
+        img.setAttribute("src", "images/jobs/" + job + "/coreskills/" + skill + "/" + Jobs[job].coreskills[skill][i] + ".png");  // Can be changed to config file and printf e.g. images/jobs/%s/coreskills/%s/%s.png
+
+        span.append(checkbox);
+        span.append(label);
+        label.append(img);
+
+        details.append(span);
+    }
+
+    div.append(details);
+    _formOwnedCoresContainer.append(div);
+}
+
+/**
+ * @param {string} job
+ * @param {string} skill
+ * @param {bool} checked
+ */
+function skillsCheckboxController(job, skill, checked) {
+    let skillsChecked = _formSkillsContainer.querySelectorAll("input[type=checkbox]:checked");
+    let skillsUnchecked = _formSkillsContainer.querySelectorAll("input[type=checkbox]:not(:checked)");
+    let skillsMax = (parseInt(_formCoreskills.value) + 1) * 3;
+
+    for (let i = 0; i < skillsUnchecked.length; i++) {
+        if (skillsChecked.length === skillsMax) {
+            skillsUnchecked[i].setAttribute("disabled", "disabled");
+        } else {
+            skillsUnchecked[i].removeAttribute("disabled");
+        }
+    }
+
+    if (checked) {
+        setFormOwnedCoresContainer(job, skill);
+    } else {
+        document.getElementById("form-owned-core-" + skill).remove();
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    setFormJob();
+});
+
+_formJob.addEventListener("change", function() {
+    let value = this.value;
+
+    if (value === "") {  // not selected
+        _formCoreskills.innerHTML = "";  // clear
+        _formCoreskills.setAttribute("disabled", "disabled");
+        _formSkillsContainer.innerHTML = "";  // clear
+        _formOwnedCoresContainer.innerHTML = "";  // clear
+    } else {  // selected
+        setFormCoreskills(value);
+        setFormSkillsContainer(value);
+        _formCoreskills.removeAttribute("disabled");
+    }
+});
+
+_formCoreskills.addEventListener("change", function() {
+    let skills = _formSkillsContainer.querySelectorAll("input[type=checkbox]");
+    
+    for (let i = 0; i < skills.length; i++) {
+        if (skills[i].checked) {
+            skills[i].checked = false;
+            skills[i].removeAttribute("disabled");
+            skills[i].dispatchEvent(new Event("change"));
+        }
+    }
+});
